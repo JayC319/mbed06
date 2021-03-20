89af6888d3a66d7f34a77e9335da500147181641
Jay <jwei2001@gmail.com>
2021-03-20 06:03:21 -0700

    add main.cpp

---
+++ 6_4_Simple_Timeout/main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+using namespace std::chrono;
+
+Timeout flipper;
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+void flip()
+{
+    led2 = !led2;
+}
+
+int main()
+{
+    led2 = 1;
+    flipper.attach(&flip, 2s);
+
+    while(1)
+    {
+        led1 = !led1;
+        ThisThread::sleep_for(200ms);
+    }
+}
